ac3f268f6944e1194fca0f447239ff438f6b1b24
stanislavlia <stanislavliashkov@gmail.com>
2024-07-16 12:55:36 +0700

    init repo

---
+++ retrieval_api.py
@@ -0,0 +1,9 @@
+from langchain_community.vectorstores import Chroma
+from langchain_core.runnables import RunnablePassthrough, RunnableParallel
+from langchain_core.output_parsers import StrOutputParser
+import chromadb
+
+CHROMA_DB_HOST="localhost"
+CHROMA_DB_PORT=8000
+
+
