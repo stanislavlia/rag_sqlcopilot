d2706944b88694d8f88157b801956cae207cc1d1
stanislavlia <stanislavliashkov@gmail.com>
2024-07-17 18:12:57 +0700

    added query execution function

--- postgres_executor_graph.py
+++ postgres_executor_graph.py
@@ -3,8 +3,17 @@ from langchain_core.runnables.base import RunnableSequence
 from langgraph.graph import END, StateGraph
 from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
 import logging
+import pandas as pd
 import psycopg2
 
+
+##=======QUERY EXECUTOR GRAPH STATE===============
+class SQLExecutorGraphState(TypedDict):
+    sql_query : Required[str]
+    sql_result : List
+    sql_result_markdown : str
+    any_errors : bool
+    error_trace : str
 
 
 class PostgresExecutor():
@@ -17,6 +26,7 @@ class PostgresExecutor():
                  pg_host : str,
                  pg_port : str,
                  pg_schema : str,
+                 limit_rows=30
                  ):
         
         self.pg_database = pg_database
@@ -25,9 +35,9 @@ class PostgresExecutor():
         self.pg_host = pg_host
         self.pg_port = pg_port
         self.pg_schema = pg_schema
+        self.limit_rows = limit_rows
 
-
-        self.db_connection, self.db_cursor = self.__connect_to_db()
+        self.db_connection = self.__connect_to_db()
         
     def __connect_to_db(self):
         try:
@@ -38,18 +48,66 @@ class PostgresExecutor():
                 host=self.pg_host,
                 port=self.pg_port
             )
-            cursor = connection.cursor()
-            cursor.execute(f"SET search_path TO {self.pg_schema};")
-            connection.commit()
+            
             logging.info(f"[PostgresExecutor] Sucessfully connected")
-            return connection, cursor
+            return connection
         except psycopg2.OperationalError as e:
             logging.error(f"[PostgresExecutor] OperationalError: {e}")
-            return None, None
+            return None
         except psycopg2.Error as e:
             logging.error(f"[PostgresExecutor] Error: {e}")
-            return None, None
+            return None
     
-        
+    def sql_result_to_markdown(self, sql_result, column_names):
 
+        df = pd.DataFrame(sql_result, column_names)
+        return df.to_markdown()
+
+
+    def _sanitize_query(self, state : SQLExecutorGraphState):
+        pass
+
+    def _execute_query(self, state : SQLExecutorGraphState):
+
+        sql_query = state["sql_query"]
+        
+        try:
+            cursor = self.db_connection.cursor()
+            cursor.execute()
+            self.db_connection.commit()
+
+            sql_result = cursor.fetchmany(size=self.limit_rows)
+            column_names = [desc[0] for desc in cursor.description]
+            sql_result_markdown = self.sql_result_to_markdown(sql_result, column_names)
+
+            new_state = state
+            new_state["any_errors"] = False
+            new_state["sql_result"] = sql_result
+            new_state["sql_result_markdown"] = sql_result_markdown
+            logging.info(f"[PostgresExecutor] Sucessfully executed query")
+
+            return new_state
+        
+        except psycopg2.Error as e:
+            logging.error(f"[PostgresExecutor] Error executing query: {e}")
+            new_state = state
+            new_state["any_errors"] = True
+            new_state["error_trace"] = str(e)
+            new_state["sql_result"] = None
+            new_state["sql_result_markdown"] = None
+
+            return new_state
+        
+        finally:
+            if cursor:
+                cursor.close()
+
+
+
+
+
+
+    
+    
+    
 
