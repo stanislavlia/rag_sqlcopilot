9472f566d71806d4cf8c20f6109a61a200fd4c88
stanislavlia <stanislavliashkov@gmail.com>
2024-07-18 13:25:44 +0700

    wrote promt template

--- sql_generator.py
+++ sql_generator.py
@@ -8,10 +8,34 @@ from copilot_base import OPENAI_API_KEY, MODEL_NAME
 import logging
 
 
-##=======SQL GENERATOR GRAPH STATE===============
+SQL_GENERATOR_PROMT = PromptTemplate("""
+    Current timestamp is {timestamp}.
+    
+    You are an expert analyst that transforms bussines questions into
+    SQL queries and answer thee questions using data-driven approach.
+    Our company is based on logistics and transportation. Your goal is to generate SQL query (postgres)
+    to answer user's question.
+                                     
+    Answer this question using provided information: {question}
+    
+    relevant DDL stamenets that describe tables you might find useful: 
+                                     {ddl_schemas}
 
+    Here's SQL queries that answer similar questions, take a look at them:
+                                     {sql_examples}
+    
+    You should also pay attention to additional tips and domain knowledge we provide:
+                                     {docs}
+
+    Make sure to generate READ-ONLY queries and your response is only valid SQL query without any 
+    additional comments.             
+
+""")
+
+##=======SQL GENERATOR GRAPH STATE===============
 class SQLGeneratorState(TypedDict):
     question : Required[str]
+    session_id : Required[int] #need for memory
     generated_sql : str
     retrieval_response : dict
     postgres_executor_response : dict
@@ -41,6 +65,24 @@ class SQLGenerator():
 
         self.retry_if_error = retry_if_error
         self.retry_if_syntax = retry_if_syntax
+
+    
+    #================Nodes implementation=================
+    def _retrieve_context(self, state : SQLGeneratorState):
+
+        question = state["question"]
+
+        retrieval_response = self.retrieval_agent.invoke({"question" : question})
+
+        new_state = state
+        new_state["retrieval_response"] = retrieval_response
+
+        return new_state
+    
+    def _build_promt(self, state : SQLGeneratorState):
+
+
+
 
         
 
